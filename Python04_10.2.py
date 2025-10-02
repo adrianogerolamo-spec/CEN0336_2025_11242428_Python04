40aa32bed7e8b839118ab2d2d98cbc2cc4b5f31f
adrianogerolamo-spec <adriano.gerolamo@usp.br>
2025-10-01 21:38:50 -0300

    python 4 ex 10 parte 2

---
+++ Python04_10.2.py
@@ -0,0 +1,12 @@
+import sys
+
+#indice inicia em 0, porem o valor 0 é o nome do programa, entao utliza-se os indices 1 e 2
+n1 = int(sys.argv[1])
+#converte-se o valor srt pra int
+n2 = int(sys.argv[2])
+
+#necessario somar mais 1 para corrigir o os valores impressos
+for i in range(n1,n2+1):
+    #imprime apenas impar
+    if i%2 != 0:
+        print(i)
